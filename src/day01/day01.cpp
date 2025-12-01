be53cdc22b84af2c131d2e570aab85aba0ff13fc
Alex McHugh <alexmchughnz@icloud.com>
2025-12-02 07:36:53 +1300

    Solved Day 1-1.

--- src/day01/day01.cpp
+++ src/day01/day01.cpp
@@ -7,13 +7,53 @@
 
 constexpr auto file = INPUT_FILE_PATH;
 
+constexpr int INITIAL_NUMBER = 50;
+constexpr int MAXIMUM_NUMBER = 99;
+
 int part_one(std::ifstream input)
 {
-    return 0;
+    int num_zeroes = 0;
+    int number = INITIAL_NUMBER;
+
+    std::string line;
+    while (input >> line)
+    {
+        const char direction = line[0];
+        const int rotation = std::stoi(line.substr(1));
+
+        switch (direction)
+        {
+        case 'L':
+            number -= rotation;
+            break;
+        case 'R':
+            number += rotation;
+            break;
+        default:
+            assert(false);
+        }
+
+        while (number < 0)
+        {
+            number += MAXIMUM_NUMBER + 1;
+        }
+        while (number > MAXIMUM_NUMBER)
+        {
+            number -= MAXIMUM_NUMBER + 1;
+        }
+
+        if (number == 0)
+        {
+            num_zeroes++;
+        }
+    }
+
+    return num_zeroes;
 }
 
 int part_two(std::ifstream input)
 {
+    (void)input;
     return 0;
 }
 
