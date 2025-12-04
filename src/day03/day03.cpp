0ab82e23b4269cb59f729ae90876d1bc072d07c1
Alex McHugh <alexmchughnz@icloud.com>
2025-12-05 06:43:18 +1300

    Scaffold Day 3.

---
+++ src/day03/day03.cpp
@@ -0,0 +1,29 @@
+#include <assert.h>
+
+#include <iostream>
+#include <fstream>
+
+#include "../helpers.hpp"
+
+int part_one(std::ifstream input)
+{
+    (void)input;
+    return 0;
+}
+
+int part_two(std::ifstream input)
+{
+    (void)input;
+    return 0;
+}
+
+int main()
+{
+    const int answer_one = part_one(std::ifstream(INPUT_FILE));
+    std::cout << "\nPart One: " <<  answer_one << std::endl;
+
+    const int answer_two = part_two(std::ifstream(INPUT_FILE));
+    std::cout << "\nPart Two: " <<  answer_two << std::endl;
+
+    return 0;
+}
